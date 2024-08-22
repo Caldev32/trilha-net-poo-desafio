75bd72545a1f7c1e2bf9c4e7f3b48428ebb649b4
Calixto <mateus.calixto1111@gmail.com>
2024-08-22 10:52:06 -0300

    feito.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia n1 = new Nokia("16988389164","Tijolão","40028922",1);
+n1.InstalarAplicativo("Whatsapp");
+Iphone i1 = new Iphone("16988389164","Button imac 456","40028922",900);
+i1.InstalarAplicativo("Safari");
